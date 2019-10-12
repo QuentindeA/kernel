7cb91dba76b9f00212bf37f820d664ac87cf3c19
Quentin de Andria <quentin@localhost.localdomain>
2019-10-12 21:50:41 +0200

    mmu fonctionnel

--- barepyOS/src/vmem.c
+++ barepyOS/src/vmem.c
@@ -2,6 +2,7 @@
 #include "vmem.h"
 #include "hw.h"
 
+
 void vmem_init(void)
 {
     //init physical memory
@@ -9,13 +10,15 @@ void vmem_init(void)
     
         //Configure coprocessor registers.
     configure_mmu_C();
-    __asm("mcr p15, 0, r0, c7, c7, 0");
         
         //activate interrupts
     ENABLE_IRQ();
         //3. De-activate and invalidate the instruction cache.
         //Then, it is possible to re-activate it when activating the MMU.
         // Here is the code for this purpose : mcr p15, 0, r0, c7, c7, 0
+    unsigned int test = vmem_translate(0x20000000, NULL);
+    test++;
+        
     start_mmu_C();
 }
 
@@ -28,7 +31,7 @@ init_kern_translation_table(void)
 
     //initialise the page table: fill the 1st-level et necessary 2nd-level tables
     
-    int** pointerToFirstLevel = (int**)(kAlloc_aligned(FIRST_LVL_TT_SIZE, 2));
+    int** pointerToFirstLevel = (int**)(kAlloc_aligned(FIRST_LVL_TT_SIZE, 14));
     
         //initialise the frame table
     
@@ -37,7 +40,7 @@ init_kern_translation_table(void)
     entryLevel2=0;
     for(entryLevel1=0; entryLevel1<=(__kernel_heap_end__/PAGE_SIZE); entryLevel1++)
     {
-        pointerToFirstLevel[entryLevel1] = (int*)kAlloc_aligned(SECON_LVL_TT_SIZE, 2);
+        pointerToFirstLevel[entryLevel1] = (int*)kAlloc_aligned(SECON_LVL_TT_SIZE, 10);
         for(entryLevel2=0; entryLevel2<SECON_LVL_TT_COUN; entryLevel2++)
         {
             pointerToFirstLevel[entryLevel1][entryLevel2] = (entryLevel1*SECON_LVL_TT_COUN*FIRST_LVL_TT_COUN+entryLevel2*FIRST_LVL_TT_COUN)|1;
@@ -48,7 +51,7 @@ init_kern_translation_table(void)
     //logical address = physical adress for any physical address between 0x20000000 and 0x20FFFFFF
     for(entryLevel1=(0x20000000/(FIRST_LVL_TT_COUN*SECON_LVL_TT_COUN)); entryLevel1<=0x20FFFFFF/(FIRST_LVL_TT_COUN*SECON_LVL_TT_COUN); entryLevel1++)
     {
-        pointerToFirstLevel[entryLevel1] = (int*)kAlloc_aligned(SECON_LVL_TT_SIZE, 2);
+        pointerToFirstLevel[entryLevel1] = (int*)kAlloc_aligned(SECON_LVL_TT_SIZE, 10);
         for(entryLevel2=0; entryLevel2<SECON_LVL_TT_COUN; entryLevel2++)
         {
             pointerToFirstLevel[entryLevel1][entryLevel2] = (entryLevel1*SECON_LVL_TT_COUN*FIRST_LVL_TT_COUN+entryLevel2*FIRST_LVL_TT_COUN)|1;
@@ -96,4 +99,53 @@ __asm volatile("mcr p15, 0, %[n], c2, c0, 2" : : [n] "r" (0));
 // Every mapped section/page is in domain 0
 
 __asm volatile("mcr p15, 0, %[r], c3, c0, 0" : : [r] "r" (0x3));
+}
+
+uint32_t
+vmem_translate(uint32_t va, struct pcb_s * process)
+{
+    uint32_t pa; /* The result */
+    /* 1st and 2nd table addresses */
+    uint32_t table_base;
+    uint32_t second_level_table;
+    /* Indexes */
+    uint32_t first_level_index;
+    uint32_t second_level_index;
+    uint32_t page_index;
+    /* Descriptors */
+    uint32_t first_level_descriptor;
+    uint32_t* first_level_descriptor_address;
+    uint32_t second_level_descriptor;
+    uint32_t* second_level_descriptor_address;
+    if (process == NULL)
+    {
+        __asm("mrc p15, 0, %[tb], c2, c0, 0" : [tb] "=r"(table_base));
+    }
+    else
+    {
+        //table_base = (uint32_t) process->page_table;
+    }
+    table_base = table_base & 0xFFFFC000;
+    /* Indexes*/
+    first_level_index = (va >> 20);
+    second_level_index = ((va << 12) >> 24);
+    page_index = (va & 0x00000FFF);
+    /* First level descriptor */
+    first_level_descriptor_address = (uint32_t*) (table_base | (first_level_index << 2));
+    first_level_descriptor = *(first_level_descriptor_address);
+    /* Translation fault*/
+    if (! (first_level_descriptor & 0x3)) {
+        return (uint32_t) FORBIDDEN_ADDRESS;
+    }
+    /* Second level descriptor */
+    second_level_table = first_level_descriptor & 0xFFFFFC00;
+    second_level_descriptor_address = (uint32_t*) (second_level_table | (second_level_index << 2));
+    second_level_descriptor = *((uint32_t*) second_level_descriptor_address);
+    /* Translation fault*/
+    if (! (second_level_descriptor & 0x3)) {
+        return (uint32_t) FORBIDDEN_ADDRESS;
+    }
+    /* Physical address */
+    pa = (second_level_descriptor & 0xFFFFF000) | page_index;
+    return pa;
 }
--- barepyOS/src/vmem.h
+++ barepyOS/src/vmem.h
@@ -7,11 +7,14 @@
 #define FIRST_LVL_TT_SIZE FIRST_LVL_TT_COUN*4 //in bytes
 #define PAGE_SIZE 4096*4 //in bytes
 
+#include "sched.h"
+
 extern uint32_t __kernel_heap_end__;
 
 void vmem_init(void);
 unsigned int init_kern_translation_table(void);
 void start_mmu_C(void);
 void configure_mmu_C(void);
+uint32_t vmem_translate(uint32_t va, struct pcb_s* process);
 
 #endif
